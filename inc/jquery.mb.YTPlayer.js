3e9faa8f1496a506151b79f74589e773391cc3ab
Baraa <bhamodi@uwaterloo.ca>
2014-10-26 21:24:09 -0700

    Updated Browser Detection to support all BlackBerry, Kindle and Windows Devices.

--- inc/jquery.mb.YTPlayer.js
+++ inc/jquery.mb.YTPlayer.js
@@ -59,11 +59,12 @@ function onYouTubePlayerAPIReady() {
 		jQuery.browser.version = jQuery.browser.majorVersion
 	}
 	jQuery.browser.android = /Android/i.test(nAgt);
-	jQuery.browser.blackberry = /BlackBerry/i.test(nAgt);
-	jQuery.browser.ios = /iPhone|iPad|iPod/i.test(nAgt);
+	jQuery.browser.blackberry = /BlackBerry|BB|PlayBook/i.test(nAgt);
+	jQuery.browser.ios = /iPhone|iPad|iPod|webOS/i.test(nAgt);
 	jQuery.browser.operaMobile = /Opera Mini/i.test(nAgt);
-	jQuery.browser.windowsMobile = /IEMobile/i.test(nAgt);
-	jQuery.browser.mobile = jQuery.browser.android || jQuery.browser.blackberry || jQuery.browser.ios || jQuery.browser.windowsMobile || jQuery.browser.operaMobile;
+	jQuery.browser.kindle = /Kindle|Silk/i.test(nAgt);
+	jQuery.browser.windowsMobile = /IEMobile|Windows Phone/i.test(nAgt);
+	jQuery.browser.mobile = jQuery.browser.android || jQuery.browser.blackberry || jQuery.browser.ios || jQuery.browser.windowsMobile || jQuery.browser.operaMobile || jQuery.browser.kindle;
 
 	ytp.isDevice = jQuery.browser.mobile;
 

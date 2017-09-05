9ba870a61c20e190ef8090f850fcfbc4111a7fc9
Matteo Bicocchi <matbicoc@gmail.com>
2017-09-05 19:14:13 +0200

    no message

--- dist/jquery.mb.YTPlayer.js
+++ dist/jquery.mb.YTPlayer.js
@@ -53,7 +53,7 @@ var getYTPVideoID = function( url ) {
 	jQuery.mbYTPlayer = {
 		name: "jquery.mb.YTPlayer",
 		version: "3.1.0",
-		build: "6364",
+		build: "6365",
 		author: "Matteo Bicocchi (pupunzi)",
 		apiKey: "",
 		defaults: {
